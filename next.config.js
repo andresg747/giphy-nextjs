202bfc6a51884533e03f0a256c498ef0148a9aa5
Andrés García <andresg747@gmail.com>
2021-07-25 15:08:09 -0300

    Removed whitespaces

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-      'media0.giphy.com', 
-      'media1.giphy.com', 
-      'media2.giphy.com', 
-      'media3.giphy.com', 
-      'media4.giphy.com', 
-      'media5.giphy.com', 
-      'media6.giphy.com', 
+      'media0.giphy.com',
+      'media1.giphy.com',
+      'media2.giphy.com',
+      'media3.giphy.com',
+      'media4.giphy.com',
+      'media5.giphy.com',
+      'media6.giphy.com',
     ],
   },
 }
